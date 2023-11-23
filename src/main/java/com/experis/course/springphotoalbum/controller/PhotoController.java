584ea87019276deddcff67d237ed8560c934fbf6
Luca <lucacassano96@gmail.com>
2023-11-23 12:58:09 +0100

    show

--- src/main/java/com/experis/course/springphotoalbum/controller/PhotoController.java
+++ src/main/java/com/experis/course/springphotoalbum/controller/PhotoController.java
@@ -3,12 +3,16 @@ package com.experis.course.springphotoalbum.controller;
 import com.experis.course.springphotoalbum.model.Photo;
 import com.experis.course.springphotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -24,6 +28,7 @@ import java.util.List;
     }
 
 
+     // LIST PHOTOS
 
         @GetMapping
         public String index(Model model) {
@@ -31,4 +36,21 @@ import java.util.List;
             model.addAttribute("photoList", photoList);
             return "photos/photos-list";
         }
+
+
+        //SHOW
+
+        @GetMapping("/show/{id}")
+        public String show(@PathVariable Integer id, Model model) {
+            Optional<Photo> result = photoRepository.findById( id );
+            if (result.isPresent()) {
+                model.addAttribute("photo", result.get());
+                return "photos/show";
+            } else {
+                throw new ResponseStatusException( HttpStatus.NOT_FOUND, "photo with id " + id + " not found" );
+            }
+
+        }
     }
+
+
